27b9749a6b538b74685d8751b892b959a1a80bc7
Stenn <stennkool@gmail.com>
2017-03-10 16:07:48 +0100

    update

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 echo "copying data"
-git clone -b release ${repository} /var/www/laravel/
-echo "pulling composer"
+mkdir /var/www/laravel
 cd /var/www/laravel
+git init
+git remote add origin ${repository}
+git fetch
+git checkout release
+echo "pulling composer"
+echo "setting laravel config file"
+cp /var/env/.env /var/www/laravel/.env
 composer update
 echo "setting permissions"
 chmod -R 777 /var/www/laravel/storage
